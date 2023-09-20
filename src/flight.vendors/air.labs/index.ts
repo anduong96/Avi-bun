523bfd8fefbdf0fff71e22a851807be5baf16237
An Duong <an.duongx@gmail.com>
2023-09-20 13:01:37 -0400

    Refactor airlabs

--- src/flight.vendors/air.labs/index.ts
+++ src/flight.vendors/air.labs/index.ts
@@ -1,6 +1,7 @@
 import { AirLabsFlightResponse, AirLabsResponse } from './types';
 
 import { ENV } from '@app/env';
+import { FlightQueryParam } from '@app/types/flight';
 import { Singleton } from '@app/lib/singleton';
 import axios from 'axios';
 
@@ -18,12 +19,14 @@ export class AirLabs extends Singleton<AirLabs>() {
    * @param args - The `args` parameter is an object that contains two properties:
    * @returns the data from the API response.
    */
-  async getFlight(args: { airlineIata: string; flightNumber: string }) {
+  async getFlight(
+    args: Pick<FlightQueryParam, 'airlineIata' | 'flightNumber'>,
+  ) {
+    type Response = AirLabsResponse<AirLabsFlightResponse>;
+
     const route = `/flight`;
     const flightIata = args.airlineIata + args.flightNumber;
-    const response = await this.client.get<
-      AirLabsResponse<AirLabsFlightResponse>
-    >(route, {
+    const response = await this.client.get<Response>(route, {
       params: {
         flight_iata: flightIata,
       },

a80e353dd6849796f5938f94721a945b21e2b641
An Duong <an.duongx@gmail.com>
2023-09-22 19:49:23 -0400

    Update logger config

--- src/lib/logger/index.ts
+++ src/lib/logger/index.ts
@@ -1,7 +1,11 @@
+import { isDev, isProd } from '@app/env';
 import * as TsLog from 'tslog';
 
 export const Logger = new TsLog.Logger({
+  name: 'App',
   type: 'pretty',
+  hideLogPositionForProduction: isProd,
+  minLevel: isDev ? 0 : 3,
   prettyLogTemplate:
-    '\n{{rawIsoStr}} {{logLevelName}} {{nameWithDelimiterPrefix}} {{filePathWithLine}}\n',
+    '\n{{rawIsoStr}} {{logLevelName}} {{name}} {{filePathWithLine}}\n',
 });

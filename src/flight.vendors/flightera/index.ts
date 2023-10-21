23946dda69b96748ef1e2ca7333896b3cf5fc1ee
An Duong <an.duongx@gmail.com>
2023-10-21 16:54:14 -0400

    Add crawl header to flightera

--- src/flight.vendors/flightera/index.ts
+++ src/flight.vendors/flightera/index.ts
@@ -7,7 +7,25 @@ import { getAircraftFromHtml } from './plane.crawl';
 export class Flightera {
   private static readonly BASE_URL = 'https://www.flightera.net';
   private static readonly client = ky.create({
-    headers: {},
+    headers: {
+      accept:
+        'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+      'accept-language': 'en-US,en;q=0.9',
+      'cache-control': 'max-age=0',
+      cookie:
+        '_pk_id.1.afcf=57f8219651488213.1694222158.; token=ovhMnNuvhLdfclvQSbzBsLRVkZlviZyVw; _pk_ref.1.afcf=%5B%22%22%2C%22%22%2C1697916660%2C%22https%3A%2F%2Fwww.google.com%2F%22%5D; _pk_ses.1.afcf=1',
+      'sec-ch-ua':
+        '"Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"macOS"',
+      'sec-fetch-dest': 'document',
+      'sec-fetch-mode': 'navigate',
+      'sec-fetch-site': 'none',
+      'sec-fetch-user': '?1',
+      'upgrade-insecure-requests': '1',
+      'user-agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
+    },
     timeout: 5 * 1000,
   });
 
@@ -26,8 +44,7 @@ export class Flightera {
     const data = await response.text();
     return getAircraftFromHtml(data);
   }
-
-  static getFlightUrl(params: {
+  static getDirectFlightUrl(params: {
     airlineIata: string;
     airlineName: string;
     departureDate: Date;
@@ -59,6 +76,17 @@ export class Flightera {
 
     return encodeURI(url);
     // https://www.flightera.net/en/flight_details/American+Airlines-New+York-London/AA100/KJFK/2023-10-19
+  }
+
+  static getFlightUrl(params: { airlineIata: string; flightNumber: string }) {
+    const route = format(
+      '%s/en/flights/%s%s',
+      this.BASE_URL,
+      params.airlineIata.toUpperCase(),
+      params.flightNumber,
+    );
+
+    return encodeURI(route);
   }
   static getPlaneUrl(tailNumber: string) {
     return encodeURI(this.BASE_URL + `/en/planes/${tailNumber}`);

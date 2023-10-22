963283d0eceb5639b0ca19fc7b6ef444fe8937af
An Duong <an.duongx@gmail.com>
2023-10-22 11:23:05 -0400

    Remove logs

--- src/flight.vendors/flight.stats/__tests__/get.random.flight.test.ts
+++ src/flight.vendors/flight.stats/__tests__/get.random.flight.test.ts
@@ -1,13 +1,10 @@
 import { describe, expect, test } from 'bun:test';
 
-import { Logger } from '@app/lib/logger';
-
 import { FlightStats } from '..';
 
 describe('Flight Stats', () => {
   test('getRandomFlight', async () => {
     const flight = await FlightStats.getRandomFlight();
-    Logger.info(flight);
     expect(flight).toBeTruthy();
     expect(flight.flightId).toBeNumber();
   });

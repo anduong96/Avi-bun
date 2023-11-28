7625b7f0d43f1856c84a0d5d70fcc1484f210ff7
An Duong <an.duongx@gmail.com>
2023-11-28 17:46:12 -0500

    Remove imperial type

--- src/@generated/graphql/enums/MeasurementType.ts
+++ src/@generated/graphql/enums/MeasurementType.ts
@@ -3,7 +3,6 @@ import { registerEnumType } from 'type-graphql';
 export enum GQL_MeasurementType {
   AMERICAN = 'AMERICAN',
   METRIC = 'METRIC',
-  IMPERIAL = 'IMPERIAL',
 }
 registerEnumType(GQL_MeasurementType, {
   name: 'MeasurementType',

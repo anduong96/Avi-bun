01b43e8a5b0e6839a227424c16bba8110a76b77f
An Duong <an.duongx@gmail.com>
2023-11-30 21:58:22 -0500

    Add populate data from weather api

--- src/services/airport.weather/get.airport.weather.ts
+++ src/services/airport.weather/get.airport.weather.ts
@@ -1,17 +1,18 @@
 import { format } from 'sys';
+import { isNil } from 'lodash';
 import moment from 'moment-timezone';
-import { isNil, omit } from 'lodash';
 import { Prisma } from '@prisma/client';
 
 import { prisma } from '@app/prisma';
 import { Sentry } from '@app/lib/sentry';
 import { Logger } from '@app/lib/logger';
 import { MetNoApi } from '@app/vendors/weather/meteorologisk';
+import { WeatherAPI } from '@app/vendors/weather/weather.api';
 
-export async function populateAirportWeather(airportIata: string) {
-  Logger.debug('populating airport weather for airport=%s', airportIata);
+async function getAirport(airportIata: string) {
   const airport = await prisma.airport.findFirstOrThrow({
     select: {
+      iata: true,
       latitude: true,
       longitude: true,
       timezone: true,
@@ -21,7 +22,12 @@ export async function populateAirportWeather(airportIata: string) {
     },
   });
 
-  if (!airport.latitude || !airport.longitude || !airport.timezone) {
+  if (
+    isNil(airport.iata) ||
+    isNil(airport.latitude) ||
+    isNil(airport.longitude) ||
+    isNil(airport.timezone)
+  ) {
     Sentry.captureException(new Error('Airport is missing location data'), {
       extra: {
         airport,
@@ -31,12 +37,18 @@ export async function populateAirportWeather(airportIata: string) {
     throw new Error('Airport location not found');
   }
 
+  return airport;
+}
+
+async function getPayloadFromMetNo(
+  airport: Awaited<ReturnType<typeof getAirport>>,
+) {
   const response = await MetNoApi.getWeatherForecast({
     latitude: airport.latitude,
     longitude: airport.longitude,
   });
 
-  const populatedIDs: string[] = [];
+  const payload: Prisma.AirportWeatherUncheckedCreateInput[] = [];
 
   for await (const entry of response.properties.timeseries) {
     const time = moment(entry.time).tz(airport.timezone);
@@ -58,9 +70,9 @@ export async function populateAirportWeather(airportIata: string) {
     const precipitationAmountMillimeter =
       nextHour?.details.precipitation_amount ?? 0;
 
-    const payload: Prisma.AirportWeatherUncheckedCreateInput = {
+    payload.push({
       airTemperatureCelsius,
-      airportIata,
+      airportIata: airport.iata!,
       date,
       hour,
       iconURL,
@@ -68,39 +80,73 @@ export async function populateAirportWeather(airportIata: string) {
       precipitationAmountMillimeter,
       status,
       updatedAt: new Date(),
+      vendor: 'MetNo',
       windFromDirectionDegrees,
       windSpeedMeterPerSecond,
       year,
-    };
-
-    const result = await prisma.airportWeather.upsert({
-      create: payload,
-      select: { id: true },
-      update: omit(payload, ['airportIata', 'date', 'hour', 'month', 'year']),
-      where: {
-        airportIata_year_month_date_hour: {
-          airportIata,
-          date,
-          hour,
-          month,
-          year,
-        },
-      },
     });
-
-    populatedIDs.push(result.id);
   }
 
-  Logger.debug(
-    'Populated %s airport weather records for airport=%s ids=%o',
-    populatedIDs.length,
-    airportIata,
-    populatedIDs,
-  );
-
-  return populatedIDs;
+  return payload;
 }
 
+async function getPayloadFromWeatherApi(
+  airport: Awaited<ReturnType<typeof getAirport>>,
+  date: Date,
+) {
+  const now = moment();
+  const isPast = now.isSameOrBefore(date);
+  const result = isPast
+    ? await WeatherAPI.getHistoricalWeather(airport, date)
+    : await WeatherAPI.getFutureWeather(airport, date);
+
+  const payload: Prisma.AirportWeatherUncheckedCreateInput[] = [];
+  for (const day of result.forecast.forecastday) {
+    for (const entry of day.hour) {
+      const iconURL = WeatherAPI.getIconUrl(entry.condition);
+      const airTemperatureCelsius = entry.temp_c;
+      const windFromDirectionDegrees = entry.wind_degree;
+      const windSpeedMeterPerSecond = entry.wind_kph;
+      const precipitationAmountMillimeter = entry.precip_mm;
+      const ts = moment(day.date).tz(airport.timezone);
+      const hour = ts.hour();
+      const month = ts.month();
+      const year = ts.year();
+      const date = ts.date();
+      const status = entry.condition.text;
+      payload.push({
+        airTemperatureCelsius,
+        airportIata: airport.iata!,
+        date,
+        hour,
+        iconURL,
+        month,
+        precipitationAmountMillimeter,
+        status,
+        updatedAt: new Date(),
+        vendor: 'WeatherApi',
+        windFromDirectionDegrees,
+        windSpeedMeterPerSecond,
+        year,
+      });
+    }
+  }
+
+  return payload;
+}
+
+/**
+ * The `getAirportWeather` function retrieves weather data for a specific airport at a given date and
+ * time, either from a database or an external API, and handles cases where the data is not found or
+ * needs to be updated.
+ * @param params - The `params` parameter is an object that contains the following properties:
+ * @param {boolean} [throwIfNotFound] - A boolean value indicating whether an error should be thrown if
+ * the airport weather is not found. If set to true, an error will be thrown if the airport weather is
+ * not found. If set to false or not provided, the function will return null if the airport weather is
+ * not found.
+ * @returns The function `getAirportWeather` returns the airport weather data for the specified
+ * parameters.
+ */
 export async function getAirportWeather(
   params: {
     airportIata: string;
@@ -152,18 +198,37 @@ export async function getAirportWeather(
     if (isBeforeInHour || lastUpdatedAtDiffHour < 1) {
       return airportWeather;
     }
-  } else if (isRequestingHistorical) {
-    throw new Error(' airport weather not found. Date is in the past');
   } else if (throwIfNotFound) {
     throw new Error('Airport weather not found');
   }
 
-  if (isRequestingHistorical) {
-    throw new Error('Airport weather not found. Date is in the past');
-  } else if (diffDate > MetNoApi.MAX_FORECAST_DAYS) {
-    throw new Error('Airport weather not found. Date is too far in the future');
-  }
+  const airport = await getAirport(airportIata);
+  const payload =
+    isRequestingHistorical || diffDate > MetNoApi.MAX_FORECAST_DAYS
+      ? await getPayloadFromWeatherApi(airport, requestingDate.toDate())
+      : await getPayloadFromMetNo(airport);
 
-  await populateAirportWeather(airportIata);
+  const [deletedResult, createdResult] = await prisma.$transaction([
+    prisma.airportWeather.deleteMany({
+      where: {
+        OR: payload.map(item => ({
+          airportIata: item.airportIata,
+          date: item.date,
+          hour: item.hour,
+          month: item.month,
+        })),
+      },
+    }),
+    prisma.airportWeather.createMany({
+      data: payload,
+    }),
+  ]);
+
+  Logger.debug(
+    'getAirportWeather:: deletedResult=%o createdResult=%o',
+    deletedResult,
+    createdResult,
+  );
+
   return getAirportWeather(params, true);
 }

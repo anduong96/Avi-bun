9da84a96492134bcf07a1d8cbe2dc80c6968bf82
An Duong <an.duongx@gmail.com>
2023-12-13 16:18:33 -0500

    Add environment to sentry

--- src/lib/sentry/index.ts
+++ src/lib/sentry/index.ts
@@ -1,3 +1,5 @@
+import { env } from 'bun';
+
 import { isEmpty } from 'lodash';
 import * as _Sentry from '@sentry/bun';
 
@@ -6,6 +8,7 @@ import { ENV, isProd, isStaging } from '@app/env';
 _Sentry.init({
   dsn: ENV.SENTRY_DSN,
   enabled: (isStaging || isProd) && !isEmpty(ENV.SENTRY_DSN),
+  environment: env.NODE_ENV,
   tracesSampleRate: 1.0,
 });
 

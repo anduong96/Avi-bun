8cf1ee6a2a8673d1e91e51cd0489bb469716d3b8
An Duong <an.duongx@gmail.com>
2023-12-22 08:35:13 -0500

    Renable Sentry

--- src/lib/sentry/index.ts
+++ src/lib/sentry/index.ts
@@ -1,11 +1,11 @@
+import { isEmpty } from 'lodash';
 import * as _Sentry from '@sentry/bun';
 
-import { ENV } from '@app/env';
+import { ENV, isDev } from '@app/env';
 
 _Sentry.init({
   dsn: ENV.SENTRY_DSN,
-  // enabled: !isDev && !isEmpty(ENV.SENTRY_DSN),
-  enabled: false,
+  enabled: !isDev && !isEmpty(ENV.SENTRY_DSN),
   environment: ENV.NODE_ENV,
   tracesSampleRate: 1.0,
 });
--- src/services/flight/flights.payload.from.aero.data.box.ts
+++ src/services/flight/flights.payload.from.aero.data.box.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import moment from 'moment-timezone';
 import { FlightStatus, Prisma } from '@prisma/client';
 

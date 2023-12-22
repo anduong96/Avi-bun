146dcee1aaf2540765da142c41627bbc0ae1adf0
An Duong <an.duongx@gmail.com>
2023-12-22 09:07:22 -0500

    Update sentry init

--- src/lib/sentry/index.ts
+++ src/lib/sentry/index.ts
@@ -3,9 +3,16 @@ import * as _Sentry from '@sentry/bun';
 
 import { ENV } from '@app/env';
 
+import { Logger } from '../logger';
+
+const isEnabled = !isEmpty(ENV.SENTRY_DSN);
+if (isEnabled) {
+  Logger.getSubLogger({ name: 'Sentry' }).info('Sentry is enabled');
+}
+
 _Sentry.init({
   dsn: ENV.SENTRY_DSN,
-  enabled: !isEmpty(ENV.SENTRY_DSN),
+  enabled: isEnabled,
   environment: ENV.NODE_ENV,
   // @see https://github.com/oven-sh/bun/issues/7472
   integrations: int => int.filter(i => !['BunServer', 'Http'].includes(i.name)),

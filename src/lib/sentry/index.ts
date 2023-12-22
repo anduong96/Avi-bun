a0226d05e8c125b0ff24cb63efce098788fc3009
An Duong <an.duongx@gmail.com>
2023-12-22 08:51:46 -0500

    Fix sentry

--- src/lib/sentry/index.ts
+++ src/lib/sentry/index.ts
@@ -7,6 +7,8 @@ _Sentry.init({
   dsn: ENV.SENTRY_DSN,
   enabled: !isDev && !isEmpty(ENV.SENTRY_DSN),
   environment: ENV.NODE_ENV,
+  // @see https://github.com/oven-sh/bun/issues/7472
+  integrations: int => int.filter(i => !['Http'].includes(i.name)),
   tracesSampleRate: 1.0,
 });
 

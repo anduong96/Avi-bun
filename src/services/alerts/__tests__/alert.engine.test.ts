df972b3770c03ed07b7ff2e6fed3e1fc621b0952
An Duong <an.duongx@gmail.com>
2024-02-03 10:50:38 -0500

    Clean up alert engine test

--- src/services/alerts/__tests__/alert.engine.test.ts
+++ src/services/alerts/__tests__/alert.engine.test.ts
@@ -1,72 +1,67 @@
 import moment from 'moment';
-import { Flight } from '@prisma/client';
-import { describe, expect, it, mock } from 'bun:test';
+import { Flight, FlightStatus } from '@prisma/client';
+import { beforeEach, describe, expect, it, mock } from 'bun:test';
 
 import { findAlertableFlightDiff } from '../alert.engine';
 
-const mockSendToTopic = () => {
-  return {
-    messageId: 1,
-  };
-};
-
-mock.module('@app/firebase', () => ({
-  firebase: {
-    messaging: () => ({
-      sendToTopic: mockSendToTopic,
-    }),
-  },
-}));
-
 describe('services::alerts::alert.engine', () => {
-  it('findAlertableFlightDiff', () => {
-    const changes = {
-      current: {
-        destinationUtcHourOffset: 5,
-        estimatedGateArrival: moment(
-          '2023-04-01 03:20',
-          'YYYY-MM-DD HH:mm',
-        ).toDate(),
-        estimatedGateDeparture: moment(
-          '2023-04-01 03:10',
-          'YYYY-MM-DD HH:mm',
-        ).toDate(),
-        originUtcHourOffset: 5,
-        status: 'CANCELLED',
-      },
-      previous: {
-        destinationUtcHourOffset: 5,
-        estimatedGateArrival: moment(
-          '2023-04-01 03:12',
-          'YYYY-MM-DD HH:mm',
-        ).toDate(),
-        estimatedGateDeparture: moment(
-          '2023-04-01 03:10',
-          'YYYY-MM-DD HH:mm',
-        ).toDate(),
-        originUtcHourOffset: 5,
-        status: 'DEPARTED',
-      },
-    };
+  const mockSendToTopic = mock(() => {
+    return Promise.resolve(1);
+  });
 
-    const diff = findAlertableFlightDiff(
-      changes.current as unknown as Flight,
-      changes.previous as unknown as Flight,
-    );
+  mock.module('@app/firebase', () => ({
+    firebase: {
+      messaging: () => ({
+        sendToTopic: mockSendToTopic,
+      }),
+    },
+  }));
 
+  const makeDate = (date: string) => moment(date, 'YYYY-MM-DD HH:mm').toDate();
+  const destinationUtcHourOffset = 5;
+  const originUtcHourOffset = 2;
+  const currentFlight = {
+    destinationUtcHourOffset,
+    estimatedGateArrival: makeDate('2023-04-01 03:20'),
+    estimatedGateDeparture: makeDate('2023-04-01 03:10'),
+    originUtcHourOffset,
+    status: FlightStatus.DEPARTED,
+  } as Flight;
+  const nextFlight: Flight = {
+    destinationUtcHourOffset,
+    estimatedGateArrival: makeDate('2023-04-01 04:20'),
+    estimatedGateDeparture: makeDate('2023-04-01 02:10'),
+    originUtcHourOffset,
+    status: FlightStatus.CANCELED,
+  } as Flight;
+
+  beforeEach(() => {
+    mockSendToTopic.mockClear();
+  });
+
+  it('test findAlertableFlightDiff', () => {
+    const diff = findAlertableFlightDiff(nextFlight, currentFlight);
     expect(diff).toEqual([
       {
         changeType: 'MODIFIED',
-        currentValue: changes.current.estimatedGateArrival,
-        description: 'Arrival Time was changed to 3:20 AM',
+        currentValue: nextFlight.estimatedGateArrival,
+        description: 'Arrival Time was changed to 4:20 AM',
         key: 'ArrivalTime',
-        previousValue: changes.previous.estimatedGateArrival,
+        previousValue: currentFlight.estimatedGateArrival,
         valueType: 'DATE',
       },
       {
         changeType: 'MODIFIED',
-        currentValue: 'CANCELLED',
-        description: 'Status was changed to CANCELLED',
+        currentValue: nextFlight.estimatedGateDeparture,
+        description: 'Departure Time was changed to 2:10 AM',
+        key: 'DepartureTime',
+        previousValue: currentFlight.estimatedGateDeparture,
+        valueType: 'DATE',
+      },
+      {
+        changeType: 'MODIFIED',
+        currentValue: 'CANCELED',
+        description: 'Status was changed to CANCELED',
         key: 'Status',
         previousValue: 'DEPARTED',
         valueType: 'STRING',
